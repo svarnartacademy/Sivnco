df922dbf5dbe97da50c89584fe552ce656d5a88b
svarnartacademy <svarnart@gmail.com>
2026-05-10 12:07:47 +0530

    fix: move inline CSS into Head to resolve React hydration mismatch on /jusamazin

--- pages/jusamazin.js
+++ pages/jusamazin.js
@@ -137,8 +137,9 @@ export default function JusAmazin() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Cormorant+Garamond:ital,wght@0,300;0,400;0,600;1,300;1,400;1,600&family=DM+Mono:ital,wght@0,400;0,500;1,400&display=swap" rel="stylesheet" />
+        <style dangerouslySetInnerHTML={{ __html: CSS }} />
       </Head>
-      <style>{CSS}</style>
+
       <div id="dot" />
 
       <nav>
